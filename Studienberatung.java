0f118e5ef8432d958ff7952c8215609c64ae7003
Dennis W <Dennis W>
2015-11-04 17:13:25 +0100

    added question hast du abitur

--- Studienberatung.java
+++ Studienberatung.java
@@ -3,6 +3,8 @@ public class Studienberatung {
 	public static void main (String[] args){
 		
 		Scanner scanner = new Scanner(System.in);
+		System.out.println("Hast du Abitur?");
+		String Abitur = scanner.next();
 		
 	}
 }
