0bd629e53cb6d3a20b103871f734bfb5cfdc6f98
Thydros <hydros@hotmail.de>
2016-10-18 21:35:10 +0200

    Add files via upload

--- Studienberatung.java
+++ Studienberatung.java
@@ -1,24 +1,32 @@
 import java.util.*;
 public class Studienberatung {
-	public static void main (String[] args){
+	public static void main(String[] args){
 		
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Hast du Abitur?");
 		String abitur = scanner.next();
+		
 
-		
-		if (abitur.equalsIgnoreCase("n")){
-			System.out.println("Hast du Fachabitur?");
-			String fachabitur = scanner.next();
-				if (fachabitur.equalsIgnoreCase("j")){
-					System.out.println("Hast du eine Berufsausbildung?");
-					String berufsausbildung = scanner.next();
-				}
-		}
 	
+	if (abitur.equalsIgnoreCase("n")){
+		System.out.println("Hast du Fachabitur?");
+		abitur = scanner.next();
+		if (abitur.equalsIgnoreCase("j")){
+			System.out.println("Hast du eine Berufsausbildung?");
+			abitur = scanner.next();
+			if(abitur.equalsIgnoreCase("j")){
+				System.out.println("Du kannst ja später noch studieren");
+			}
+			else {
+				System.out.println("Besser noch studieren");
+			}
 		
-		else if (abitur.equalsIgnoreCase("j")){
-			System.out.println("Bist du wissbegierig?");
-		}
 	}
-}
+	}
+	else if (abitur.equalsIgnoreCase("j")){
+			System.out.println("Bist du wissbegierig?");
+			String wiss = scanner.next();
+		}
+			
+		
+	}}
